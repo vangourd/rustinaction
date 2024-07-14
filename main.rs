0b9c87ecfd9437eb2e9bb04fcc1e44962e85e541
Brian Logan <mail@brianjlogan.com>
2024-07-13 22:50:50 -0400

    3.2 modeling struct

--- main.rs
+++ main.rs
@@ -1,18 +1,46 @@
+#![allow(unused_variables)]
+
 #[derive(Debug)]
-struct File {
+struct File{
     name: String,
     data: Vec<u8>,
 }
 
+fn open(f: &mut File) -> bool {
+    true
+}
+
+fn close(f: &mut File) -> bool {
+    true
+}
+
+fn read(
+    f: &File,
+    save_to: &mut Vec<u8>,
+) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
+}
+
 fn main() {
-    let f1 = File {
-        name: String::from("f1.txt"),
-        data: Vec::new(),
+    let mut f2 = File {
+        name: String::from("2.txt"),
+            data: vec![114, 117, 115, 116, 33],
     };
 
-    let f1_name = &f1.name;
-    let f1_length = &f1.data.len();
+    let mut buffer: Vec<u8> = vec![];
 
-    println!("{:?}", f1);
-    println!("{} is {} bytes long", f1_name, f1_length);
+    open(&mut f2);
+    let f2_length = read(&f2, &mut buffer);
+    close(&mut f2);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{} is {} bytes long", &f2.name, f2_length);
+    println!("{}", text)
 }
