a8d84738e1937ca0fe8d35612cb74048449287fa
Brian Logan <mail@brianjlogan.com>
2024-07-13 22:24:36 -0400

    3.2 modeling files with struct

--- main.rs
+++ main.rs
@@ -12,12 +12,12 @@ fn close(f: &mut File) -> bool {
 
 #[allow(dead_code)]
 fn read(f: &mut File,
-            save_to: &mut Vec<u8>) -> ! (
+            save_to: &mut Vec<u8>) -> ! {
     unimplemented!()
-)
+}
 
 fn main() {
-    let mut f1 = File::from("f1.txt";)
+    let mut f1 = File::from("f1.txt");
     open(&mut f1);
     //read(f1, vec![]);
     close(&mut f1);
