755c5c28bddbb661b30274ca6e1687da40f91f0e
Brian Logan <mail@brianjlogan.com>
2024-07-13 22:42:26 -0400

    3.2

--- main.rs
+++ main.rs
@@ -1,24 +1,18 @@
-#![allow(unused_variables)]
-
-type File = String;
-
-fn open(f: &mut File) -> bool {
-    true
-}
-
-fn close(f: &mut File) -> bool {
-    true
-}
-
-#[allow(dead_code)]
-fn read(f: &mut File,
-            save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
 }
 
 fn main() {
-    let mut f1 = File::from("f1.txt");
-    open(&mut f1);
-    //read(f1, vec![]);
-    close(&mut f1);
+    let f1 = File {
+        name: String::from("f1.txt"),
+        data: Vec::new(),
+    };
+
+    let f1_name = &f1.name;
+    let f1_length = &f1.data.len();
+
+    println!("{:?}", f1);
+    println!("{} is {} bytes long", f1_name, f1_length);
 }
