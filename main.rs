fce6e3ee65f4304db7125df18aa1b0c8f103575b
Brian Logan <mail@brianlogan.com>
2024-09-20 21:21:24 -0400

    Listing 3.10

--- main.rs
+++ main.rs
@@ -1,69 +1,39 @@
-use rand::prelude::*; // #1
-
-fn one_in(denominator: u32) -> bool { // #2
-    thread_rng().gen_ratio(1, denominator) // #3
+#[derive(Debug)] // #1
+enum Event {
+    Update, //# 2
+    Delete, // #2
+    Unknown, // #2
 }
 
-#[derive(Debug)]
-struct File {
-    name: String,
-    data: Vec<u8>,
+type Message = String; // #3
+
+fn parse_log(line: &str) -> (Event, Message) { // #4
+    let parts: Vec<_> = line  // #5
+                        .splitn(2, ' ')
+                        .collect(); // #6
+    if parts.len() == 1 {
+        return (Event::Unknown, String::from(line))
+    }
+    
+    let event = parts[0]; // #8
+    let rest = String::from(parts[1]); // #8
+
+    match event {
+        "UPDATE" | "update" => (Event::Update, rest), // #9
+        "DELETE" | "delete" => (Event::Delete, rest), // #9
+        _ => (Event::Unknown, String::from(line)), // #10
+    }
+
 }
-
-impl File {
-    fn new(name: &str) -> File {
-        File {
-            name: String::from(name),
-            data: Vec::new()
-        }
-    }
-
-    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
-        let mut f = File::new(name);
-        f.data = data.clone();
-        f
-    }
-
-    fn read(
-        self: &File,
-        save_to: &mut Vec<u8>,
-    ) -> Result<usize, String> { // #5
-        let mut tmp = self.data.clone();
-        let read_length = tmp.len();
-        save_to.reserve(read_length);
-        save_to.append(&mut tmp);
-        Ok(read_length) // #6
-    }
-}
-
-fn open(f: File) -> Result<File, String> {
-    if one_in(10_000) { // #7
-        let err_msg = String::from("Interuptted by signal!");
-        return Err(err_msg);
-    }
-    Ok(f)
-}
-
-fn close(f: File) -> Result<File, String> {
-    if one_in(100_000) { // #8
-        let err_msg = String::from("Interrupted by signal!");
-        return Err(err_msg);
-    }
-    Ok(f)
-}
-
 
 fn main() {
-    let f4_data: Vec<u8> = vec![114, 117, 115, 116,33];
-    let mut f4 = File::new_with_data("4.txt", &f4_data);
-    let mut buffer: Vec<u8> = vec![];
-    
-    f4 = open(f4).unwrap(); // #9
-    let f4_length = f4.read(&mut buffer).unwrap(); // #9
-    f4 = close(f4).unwrap(); // #9
+    let log = "BEGIN Transaction XK342 
+    UPDATE 234:LS/32221 {\"price\": 31.00} -> {\"price\": 40.00} 
+    DELETE 342:L0/22111";
 
-    let text = String::from_utf8_lossy(&buffer);
-    println!("{:?}", f4);
-    println!("{} is {} bytes long", &f4.name, f4_length);
-    println!("{}", text);
+    for line in log.lines() {
+        let parse_result = parse_log(line);
+        println!("{:?}", parse_result);
+    }
+    
 }
