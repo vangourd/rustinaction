6d01e858be9a9a8268b99a3b9d4316a18ed129fb
Brian Logan <mail@brianlogan.com>
2024-10-01 17:11:20 -0400

    Listing 3.12

--- main.rs
+++ main.rs
@@ -1,66 +1,24 @@
-#[derive(Debug, PartialEq)]
-enum FileState {
-    Open,
-    Closed,
-}
+#![allow(unused_variables)] // 1
 
 #[derive(Debug)]
-struct File {
-    name: String,
-    data: Vec<u8>,
-    state: FileState,
-}
+struct File; // 2
 
-impl File {
-    fn new(name: &str) -> File{
-        File {
-            name: String::from(name),
-            data: Vec::new(),
-            state: FileState::Closed,
-        }
-    }
-
-    fn read(
-        self: &File,
+trait Read {
+    fn read (
+        self: &Self,
         save_to: &mut Vec<u8>,
-    ) -> Result<usize, String> {
-        if self.state != FileState::Open {
-            return Err(String::from("File must be open for reading"));
-        }
-        let mut tmp = self.data.clone();
-        let read_length = tmp.len();
-        save_to.reserve(read_length);
-        save_to.append(&mut tmp);
-        Ok(read_length)
+    ) -> Result<usize, String>; // 4
+}
+
+impl Read for File {
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        Ok(0) // 5
     }
-}
-
-fn open(mut f: File) -> Result<File, String> {
-    f.state = FileState::Open;
-    Ok(f)
-}
-
-fn close(mut f: File) -> Result<File, String> {
-    f.state = FileState::Closed;
-    Ok(f)
 }
 
 fn main() {
-    let mut f5 = File::new("5.txt");
-
-    let mut buffer: Vec<u8> = vec![];
-
-    if f5.read(&mut buffer).is_err() {
-        println!("Error checking is working");
-    }
-
-    f5 = open(f5).unwrap();
-    let f5_length = f5.read(&mut buffer).unwrap();
-    f5 = close(f5).unwrap();
-
-    let text = String::from_utf8_lossy(&buffer);
-
-    println!("{:?}", f5);
-    println!("{} is {} bytes long", &f5.name, f5_length);
-    println!("{}", text);
+    let f = File{};
+    let mut buffer = vec!();
+    let n_bytes = f.read(&mut buffer).unwrap();
+    println!("{} byte(s) read from {:?}", n_bytes, f);
 }
