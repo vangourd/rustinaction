4fd975b891f83df75da2ca4aa9d7bc94893aa758
Brian Logan <mail@brianlogan.com>
2024-11-11 10:01:41 -0500

    Listing 4.7

--- main.rs
+++ main.rs
@@ -1,13 +1,33 @@
-fn use_value (_val: Demo) {
+#![allow(unused_variables)]
 
+#[derive(Debug)]
+struct CubeSat {
+    id: u64,
 }
 
-struct Demo {
-    a: i32,
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
 }
 
-fn main () {
-    let demo = Demo { a: 123 };
-    use_value(demo);
-    println!("{}", demo.a);
+fn check_status(sat_id: CubeSat) -> CubeSat {
+    println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
+    sat_id
+}
+
+fn main() {
+    let sat_a = CubeSat { id: 0 };
+    let sat_b = CubeSat { id: 1 };
+    let sat_c = CubeSat { id: 2 };
+
+    let sat_a = check_status(sat_a); // 1
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
+
+    // "waiting" ...
+
+    let sat_a = check_status(sat_a);
+    let sat_b = check_status(sat_b);
+    let sat_c = check_status(sat_c);
+
 }
