1e2afee6cf3fbf0b99077f5b106eadc006a57166
Brian Logan <mail@brianlogan.com>
2024-11-15 15:03:20 -0700

    Listing 4.17

--- main.rs
+++ main.rs
@@ -1,84 +1,39 @@
-#![allow(unused_variables)]
-
-#[derive(Debug)]
+#[derive(Clone,Debug)]
 struct CubeSat {
     id: u64,
 }
 
+impl Copy for CubeSat { }
+
+impl Clone for CubeSat { // 1
+    fn clone(&self) -> Self {
+        CubeSat { id: self.id }  // 2
+    }
+}
+
 #[derive(Debug)]
-struct Mailbox {
-    messages: Vec<Message>,
+enum StatusMessage {
+    Ok,
 }
 
-#[derive(Debug)]
-struct Message {
-    to: u64,
-    content: String,
-}
+impl Copy for StatusMessage { }
 
-struct GroundStation {}
-
-impl Mailbox {
-    fn post(&mut self, msg: Message) {
-        self.messages.push(msg);
-    }
-
-    fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
-        for i in 0..self.messages.len() {
-            if self.messages[i].to == recipient.id {
-                let msg = self.messages.remove(i);
-                return Some(msg);
-            }
-        }
-
-        None
+impl Clone for StatusMessage { 
+    fn clone(&self) -> Self {
+        *self                   // 3
     }
 }
 
-impl GroundStation {
-    fn connect(&self, sat_id: u64) -> CubeSat {
-        CubeSat {
-            id: sat_id,
-        }
-    }
-
-    fn send(&self, mailbox: &mut Mailbox, msg: Message) {
-        mailbox.post(msg);
-    }
-}
-
-impl CubeSat {
-    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
-        mailbox.deliver(&self)
-    }
-}
-
-fn fetch_sat_ids() -> Vec<u64> {
-    vec![1,2,3]
+fn check_status(_sat_id: CubeSat) -> StatusMessage {
+    StatusMessage::Ok
 }
 
 fn main() {
-    let mut mail = Mailbox { messages: vec![] };
+    let sat_a = CubeSat { id: 0 };
 
-    let base = GroundStation {};
+    let a_status = check_status(sat_a);
+    println!("a: {:?}", a_status);
 
-    let sat_ids = fetch_sat_ids();
-
-    for sat_id in sat_ids {
-        let sat = base.connect(sat_id);
-        let msg = Message {
-            to: sat_id,
-            content: String::from("hello")
-        };
-        base.send(&mut mail, msg);
-    }
-
-    let sat_ids = fetch_sat_ids();
-
-    for sat_id in sat_ids {
-        let sat = base.connect(sat_id);
-
-        let msg = sat.recv(&mut mail);
-        println!("{:?}: {:?}",sat,msg);
-    }
+    let a_status = check_status(sat_a); // 1
+    println!("a: {:?}", a_status);
 }
