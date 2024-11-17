602e2063245180354d5654cfc64750ee6b12ec6b
Brian Logan <mail@brianlogan.com>
2024-11-17 10:27:49 -0500

    Listing 5.5

--- main.rs
+++ main.rs
@@ -1,7 +1,6 @@
+#[allow(arithmetic_overflow)]
 fn main() {
-    let a: u16 = 50115;
-    let b: i16 = -15421;
-
-    println!("a: {:016b} {}",a ,a);
-    println!("b: {:016b} {}", b ,b);
+    let (a, b) = (200, 200);
+    let c: u8 = a + b;              // 2
+    println!("200 + 200 = {}", c);
 }
