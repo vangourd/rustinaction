617bed37cd7df40ff9d27f1febbc0b3c2785a765
Brian Logan <mail@brianlogan.com>
2024-11-16 21:10:33 -0700

    Listing 5.1

--- main.rs
+++ main.rs
@@ -1,30 +1,7 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-
-#[derive(Debug)]
-struct GroundStation {
-    radio_freq: f64 // Mhz
-}
-
 fn main() {
-    let base: Rc<RefCell<GroundStation>> = 
-        Rc::new(RefCell::new(
-            GroundStation {
-                radio_freq: 87.64
-            }
-        ));
+    let a: u16 = 50115;
+    let b: i16 = -15421;
 
-    println!("base: {:?}", base);
-
-    {
-        let mut base_2 = base.borrow_mut();
-        base_2.radio_freq -= 12.34;
-        println!("base_2: {:?}", base_2);
-    }
-
-    let mut base_3 = base.borrow_mut();
-    base_3.radio_freq += 43.21;
-
-    println!("base: {:?}", base);
-    println!("base_3: {:?}", base_3);
+    println!("a: {:016b} {}",a ,a);
+    println!("b: {:016b} {}", b ,b);
 }
